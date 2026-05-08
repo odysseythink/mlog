1611007cf26d1022cf3ee9a0530064d3e809e8f9
ranwei693532 <ranwei693532@163.com>
2026-05-08 23:41:54 +0800

    test(mode): add global function routing tests for both modes

--- mode_test.go
+++ mode_test.go
@@ -5,13 +5,27 @@ import (
 	"testing"
 )
 
+func resetMode() {
+	modeSetOnce = sync.Once{}
+	logMode.Store(0)
+}
+
+func setStructured() {
+	resetMode()
+	SetLogMode(LogModeStructured)
+}
+
+func setPrintf() {
+	resetMode()
+	// printf is default (0)
+}
+
 func TestSetLogMode(t *testing.T) {
 	// Save original state
 	origMode := logMode.Load()
 
 	// Reset for testing
-	modeSetOnce = sync.Once{}
-	logMode.Store(0)
+	resetMode()
 
 	SetLogMode(LogModeStructured)
 	if got := getMode(); got != LogModeStructured {
@@ -25,7 +39,7 @@ func TestSetLogMode(t *testing.T) {
 	}
 
 	// Restore
-	modeSetOnce = sync.Once{}
+	resetMode()
 	logMode.Store(origMode)
 }
 
@@ -54,34 +68,27 @@ func TestWith(t *testing.T) {
 
 func TestGlobalInfoStructuredMode(t *testing.T) {
 	// Ensure structured mode
-	modeSetOnce = sync.Once{}
-	logMode.Store(0)
-	SetLogMode(LogModeStructured)
+	setStructured()
 
 	// Should not panic in structured mode
 	Info("test message", String("key", "value"))
 }
 
 func TestGlobalInfofStructuredMode(t *testing.T) {
-	modeSetOnce = sync.Once{}
-	logMode.Store(0)
-	SetLogMode(LogModeStructured)
+	setStructured()
 
 	Infof("formatted %s", "message")
 }
 
 func TestLoggerInfoStructuredMode(t *testing.T) {
-	modeSetOnce = sync.Once{}
-	logMode.Store(0)
-	SetLogMode(LogModeStructured)
+	setStructured()
 
 	logger := With(String("svc", "test"))
 	logger.Info("request", String("path", "/api"))
 }
 
 func TestLoggerPrintfMode(t *testing.T) {
-	modeSetOnce = sync.Once{}
-	logMode.Store(0)
+	setPrintf()
 	// Default is printf mode, no SetLogMode call needed
 
 	// Avoid writing to real files to prevent O_EXCL collisions
@@ -92,4 +99,106 @@ func TestLoggerPrintfMode(t *testing.T) {
 
 	logger := With(String("svc", "test"))
 	logger.Info("request") // Should work in printf mode, ignoring fields
+}
+
+func TestInfoStructured(t *testing.T) {
+	setStructured()
+
+	origTextSinks := TextSinks
+	TextSinks = nil
+	defer func() { TextSinks = origTextSinks }()
+
+	// Empty args
+	infoStructured(1, Severity_Info)
+
+	// String msg only
+	infoStructured(1, Severity_Info, "hello")
+
+	// String msg + fields
+	infoStructured(1, Severity_Info, "hello", String("k", "v"))
+
+	// Non-string first arg falls back to fmt.Sprint
+	infoStructured(1, Severity_Info, 123)
+}
+
+func TestInfofStructured(t *testing.T) {
+	setStructured()
+
+	origTextSinks := TextSinks
+	TextSinks = nil
+	defer func() { TextSinks = origTextSinks }()
+
+	infofStructured(1, Severity_Info, "hello %s", "world")
+}
+
+func TestGlobalFunctionsStructured(t *testing.T) {
+	setStructured()
+
+	origTextSinks := TextSinks
+	TextSinks = nil
+	defer func() { TextSinks = origTextSinks }()
+
+	Debug("debug msg")
+	Debugf("debug %s", "msg")
+	Info("info msg")
+	Infof("info %s", "msg")
+	Warning("warn msg")
+	Warningf("warn %s", "msg")
+	Error("error msg")
+	Errorf("error %s", "msg")
+}
+
+func TestGlobalFunctionsPrintf(t *testing.T) {
+	setPrintf()
+
+	origTextSinks := TextSinks
+	TextSinks = nil
+	defer func() { TextSinks = origTextSinks }()
+
+	Debug("debug msg")
+	Debugf("debug %s", "msg")
+	Info("info msg")
+	Infof("info %s", "msg")
+	Warning("warn msg")
+	Warningf("warn %s", "msg")
+	Error("error msg")
+	Errorf("error %s", "msg")
+}
+
+func TestVerboseStructured(t *testing.T) {
+	setStructured()
+
+	origTextSinks := TextSinks
+	TextSinks = nil
+	defer func() { TextSinks = origTextSinks }()
+
+	v := Verbose(true)
+	v.Info("verbose info")
+	v.Infof("verbose %s", "infof")
+	v.Infoln("verbose infoln")
+
+	// Verbose(false) should be no-ops
+	v2 := Verbose(false)
+	v2.Info("should not log")
+	v2.Infof("should %s", "not log")
+	v2.Infoln("should not log")
+}
+
+func TestVerbosePrintf(t *testing.T) {
+	setPrintf()
+
+	origTextSinks := TextSinks
+	TextSinks = nil
+	defer func() { TextSinks = origTextSinks }()
+
+	v := Verbose(true)
+	v.Info("verbose info")
+	v.Infof("verbose %s", "infof")
+	v.Infoln("verbose infoln")
+
+	// Verbose(false) should be no-ops
+	v2 := Verbose(false)
+	v2.Info("should not log")
+	v2.Infof("should %s", "not log")
+	v2.Infoln("should not log")
 }
